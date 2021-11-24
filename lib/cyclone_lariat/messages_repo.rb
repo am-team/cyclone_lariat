afb414e145269002a4b42c0336827bd06351825c
Kirill Drozdov <kirill.dzv@gmail.com>
2021-11-24 10:40:56 +0500

    Refactored messages repo

--- lib/cyclone_lariat/messages_repo.rb
+++ lib/cyclone_lariat/messages_repo.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'event'
-require_relative 'utils/hash'
+require_relative 'messages_mapper'
 
 module CycloneLariat
   class MessagesRepo
@@ -12,17 +12,7 @@ module CycloneLariat
     end
 
     def create(msg)
-      dataset.insert(
-        uuid: msg.uuid,
-        kind: msg.kind,
-        type: msg.type,
-        publisher: msg.publisher,
-        data: JSON.generate(msg.data),
-        client_error_message: msg.client_error&.message,
-        client_error_details: JSON.generate(msg.client_error&.details),
-        version: msg.version,
-        sent_at: msg.sent_at
-      )
+      dataset.insert MessagesMapper.to_row(msg)
     end
 
     def exists?(uuid:)
@@ -37,35 +27,20 @@ module CycloneLariat
     end
 
     def find(uuid:)
-      raw = dataset.where(uuid: uuid).first
-      return nil unless raw
-
-      raw[:data] = hash_from_json_column(raw[:data])
-
-      if raw[:client_error_details]
-        raw[:client_error_details] = hash_from_json_column(raw[:client_error_details])
-      end
-      build raw
+      row = dataset.where(uuid: uuid).first
+      build MessagesMapper.from_row(row)
     end
 
     def each_unprocessed
-      dataset.where(processed_at: nil).each do |raw|
-        raw[:data] = hash_from_json_column(raw[:data])
-        if raw[:client_error_details]
-          raw[:client_error_details] = hash_from_json_column(raw[:client_error_details])
-        end
-        msg = build raw
+      dataset.where(processed_at: nil).each do |row|
+        msg = build MessagesMapper.from_row(row)
         yield(msg)
       end
     end
 
     def each_with_client_errors
-      dataset.where { (processed_at !~ nil) & (client_error_message !~ nil) }.each do |raw|
-        raw[:data] = hash_from_json_column(raw[:data])
-        if raw[:client_error_details]
-          raw[:client_error_details] = hash_from_json_column(raw[:client_error_details])
-        end
-        msg = build raw
+      dataset.where { (processed_at !~ nil) & (client_error_message !~ nil) }.each do |row|
+        msg = build MessagesMapper.from_row(row)
         yield(msg)
       end
     end
@@ -78,21 +53,6 @@ module CycloneLariat
       when 'command' then Command.wrap raw
       else raise ArgumentError, "Unknown kind `#{kind}` of message"
       end
-    end
-
-    def hash_from_json_column(data)
-      return JSON.parse(data, symbolize_names: true) if data.is_a?(String)
-
-      if pg_json_extension_enabled?
-        return Utils::Hash.deep_symbolize_keys(data.to_h)   if data.is_a?(Sequel::Postgres::JSONHash)
-        return JSON.parse(data.to_s, symbolize_names: true) if data.is_a?(Sequel::Postgres::JSONString)
-      end
-
-      raise ArgumentError, "Unknown type of `#{data}`"
-    end
-
-    def pg_json_extension_enabled?
-      Object.const_defined?('Sequel::Postgres::JSONHash')
     end
   end
 end

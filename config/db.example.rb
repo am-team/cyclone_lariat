2ad7455a5f9eabb2a02f8255a76f341c07595de8
Captain Philipp <ph-s@mail.ru>
2021-12-16 15:34:26 +0700

    Update

--- config/db.example.rb
+++ config/db.example.rb
@@ -5,5 +5,5 @@ DB_CONF = {
   host: ENV.fetch('DB_HOST', 'host'),
   username: ENV.fetch('DB_USER', 'postgres'),
   password: ENV.fetch('DB_PASSWORD', 'password'),
-  database: ENV.fetch('DB_NAME', 'cyclone-lariat-test'
+  database: ENV.fetch('DB_NAME', 'cyclone-lariat-test')
 }.freeze
